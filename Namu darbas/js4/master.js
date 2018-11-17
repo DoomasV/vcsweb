06c1f567ecaa7891b1238ff3c26b684d19a64907
Dominykas <vaisvis@gmail.com>
2018-11-17 19:56:18 +0200

    sutvarkyti atitraukimai master.js faile.

--- Namu darbas/js4/master.js
+++ Namu darbas/js4/master.js
@@ -1,11 +1,11 @@
 function amziausTikrinimas (age){
-    if(isNaN(age)){
+  if(isNaN(age)){
     document.write("Prasau iveskite skaiciu");
-    }
-    if(age < 18){
+  }
+  if(age < 18){
     document.write("Jus negalite pateikti i svetaine");
-    }
-    else if (age >= 18){
+  }
+  else if (age >= 18){
     document.write("Sveiki prisijunge prie svetaines");
   }
 }
