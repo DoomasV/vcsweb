eacc3c0145f8eaab013da7b9e1b4d67c9fce7253
Dominykas <vaisvis@gmail.com>
2018-11-26 21:13:48 +0200

    pridetas namu darbas php02

---
+++ Namu darbas/php02/index02.php
@@ -0,0 +1,16 @@
+<?php
+  $country = 'Lietuva';
+  $city = 'Vilnius';
+  $populiacija = 574221;
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php echo $city . ' - Lietuvos sostine'; ?></p>
+    <p><?php echo "$city ir $country sparciai auga"; ?></p>
+  </body>
+</html>
