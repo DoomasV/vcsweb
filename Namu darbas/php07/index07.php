ef94cd266d40aced9276cc7f7f37ad399b99b55c
DoomasV <vaisvis@gmail.com>
2018-11-28 12:54:34 +0200

    pridetas namu darbas php07.

---
+++ Namu darbas/php07/index07.php
@@ -0,0 +1,33 @@
+<?php
+  $cities4 = [
+    'Tokijas',
+    'Vašingtonas',
+    'Maskva',
+    'Londonas'
+  ]
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <?php
+        // $miestai = count($cities4)
+        for ($i=0; $i < count($cities4); $i++) {
+          echo "<li>$cities4[$i]</li>";
+        };
+      ?>
+    </ul>
+    <ul>
+      <?php
+        foreach ($cities4 as $miestas) {
+          echo "<li>$miestas</li>";
+        }
+      ?>
+    </ul>
+  </body>
+</html>
