85b386ca33e7313388f492b2263f19810f040caa
DoomasV <vaisvis@gmail.com>
2018-11-28 17:03:21 +0200

    pridetas namu darbas php09.

---
+++ Namu darbas/php09/index09.php
@@ -0,0 +1,36 @@
+<?php
+  $ceu = array(
+    "Italija"=>"Roma",
+    "Liuksemburgas"=>"Liuksemburgas",
+    "Belgija"=>"Briuselis",
+    "Danija"=>"Kopenhaga",
+    "Suomija"=>"Helsinkis",
+    "Prancūzija"=>"Paryžius",
+    "Slovakija"=>"Bratislava",
+    "Slovenija"=>"Liublijana",
+    "Vokietija"=>"Berlynas",
+    "Graikija"=>"Atėnai",
+    "Airija"=>"Dublinas",
+    "Nyderlandai"=>"Amsterdamas",
+    "Portugalija"=>"Lisabona",
+    "Ispanija"=>"Madridas",
+    "Švedija"=>"Stokholmas",
+    "DidžiojiBritanija"=>"Londonas",
+    "Kipras"=>"Nikosija",
+    "Lietuva"=>"Vilnius",
+    "Čekija"=>"Praha",
+    "Estija"=>"Talinas",
+    "Vengrija"=>"Budapeštas",
+    "Latvija"=>"Ryga",
+    "Malta"=>"Valeta",
+    "Austrija"=>"Viena",
+    "Bulgarija"=>"Sofija",
+    "Rumunija"=>"Bukareštas",
+    "Lenkija"=>"Varšuva"
+  );
+  asort($ceu);
+  foreach($ceu as $salis => $sostine) {
+    echo ($salis.' '.$sostine);
+    echo '<br />';
+  };
+?>
