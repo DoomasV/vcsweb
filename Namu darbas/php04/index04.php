24c32c1cede25c1a86a63fbdb6f0b7eb1883181b
DoomasV <vaisvis@gmail.com>
2018-12-02 17:17:44 +0200

    pataisyta klaida php04.

--- Namu darbas/php04/index04.php
+++ Namu darbas/php04/index04.php
@@ -8,7 +8,7 @@
     'vasingtonas' => 0.6,
     'maskva' => 11.5,
   ];
-  $cities2 ['londonas'] = '8.6';
+  $cities2 ['londonas'] = 8.6;
   print_r($cities2);
 ?>
 
@@ -20,10 +20,10 @@
   </head>
   <body>
     <ul>
-      <li><?php echo($cities[1]) ?></li>
+      <li><?php echo($cities[1]); ?></li>
     </ul>
     <ul>
-      <li><?php echo('Gyventoju skaicius: '. $cities2['tokijas'].' mln.'); ?></li>
+      <li><?php echo('Gyventoju skaicius: '.$cities2['tokijas'].' mln.'); ?></li>
     </ul>
   </body>
 </html>
