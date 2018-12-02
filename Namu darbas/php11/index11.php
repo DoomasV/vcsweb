1b7a3e31bdc15ae22dbb2e2c13f5dd57e899fad1
DoomasV <vaisvis@gmail.com>
2018-12-02 19:06:53 +0200

    pataisytas php11.

--- Namu darbas/php11/index11.php
+++ Namu darbas/php11/index11.php
@@ -5,14 +5,14 @@
     <title></title>
   </head>
   <body>
-      <?php if(!isset($_POST['Submit']) && empty($_POST['length']) && empty($_POST['width'])) { ?>
+      <?php if(!isset($_POST['Submit']) && empty($_POST['length']) || empty($_POST['width'])) { ?>
           <form action="" method="POST">
             Input length: <input type="text" name="length">
             Input width: <input type="text" name="width">
             <input type="submit" value="Submit">
           </form>
       <?php } else {
-                  echo('Area: '.$area = $_POST['length'] * $_POST['width']);
+                  echo 'Area: '.$area = $_POST['length'] * $_POST['width'] ;
               };
       ?>
   </body>
