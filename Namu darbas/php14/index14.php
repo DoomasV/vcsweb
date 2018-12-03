b37cad37788911724b6a5940155e637d12d4b03d
DoomasV <vaisvis@gmail.com>
2018-12-03 16:15:12 +0200

    pataisytas php14.

--- Namu darbas/php14/index14.php
+++ Namu darbas/php14/index14.php
@@ -1,8 +1,10 @@
 <?php
   $dabartiniaiMetai = date('Y');
-  $sukurimoMetai = 2015;
+  $sukurimoMetai = 2020;
   function sukurimoMetai($x, $y) {
     if ($x == $y ) {
+      return $x;
+    } elseif ($x < $y) {
       return $x;
     } else {
       return $y.'-'.$x;
